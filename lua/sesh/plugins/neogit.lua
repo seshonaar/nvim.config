4990ad41b2976e1ad714602e9d021d8390ed407d
seshonaar <andrei.balint@gmail.com>
2024-08-20 22:40:17 +0300

    add neogit log command to open diff from selected commit to HEAD

--- lua/sesh/plugins/neogit.lua
+++ lua/sesh/plugins/neogit.lua
@@ -1,8 +1,8 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "seshonaar/diffview.nvim",        -- optional - Diff integration
+    "nvim-lua/plenary.nvim",        -- required
+    "seshonaar/diffview.nvim",      -- optional - Diff integration
     -- Only one of these is needed, not both.
     "nvim-telescope/telescope.nvim" -- optional
   },
@@ -21,40 +21,71 @@ return {
       graph_style = "unicode",
     }
 
-    vim.keymap.set( {"n"} , "<leader>gg",
+    -- Define the function to open DiffView from the selected commit to HEAD
+    local function open_diffview_for_selected_commit()
+      -- Get the current line under the cursor (this should be the commit hash line)
+      local current_line = vim.api.nvim_get_current_line()
+
+      -- Extract the commit hash from the line (this assumes the commit hash is the first word)
+      local commit_hash = current_line:match("%w+")
+
+      if commit_hash then
+        -- Open DiffView comparing the selected commit with HEAD
+        vim.cmd('DiffviewOpen ' .. commit_hash .. '..HEAD')
+      else
+        print("No commit hash found on the current line")
+      end
+    end
+
+    -- Function to set the buffer-local key mapping for the Neogit log view
+    local function set_neogit_log_mappings(bufnr)
+      vim.keymap.set({ "n" }, "<leader>d",
+        open_diffview_for_selected_commit,
+        { silent = true, noremap = true, buffer = bufnr })
+    end
+
+    -- Auto-command to apply the mapping only in the Neogit log view
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "NeogitLogView", -- Neogit log buffer file type
+      callback = function(args)
+        set_neogit_log_mappings(args.buf)
+      end
+    })
+
+    vim.keymap.set({ "n" }, "<leader>gg",
       function()
         vim.cmd("Neogit")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
 
-    vim.keymap.set( {"n"} , "<leader>gc",
+    vim.keymap.set({ "n" }, "<leader>gc",
       function()
         vim.cmd("Neogit commit")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
 
-    vim.keymap.set( {"n"} , "<leader>gp",
+    vim.keymap.set({ "n" }, "<leader>gp",
       function()
         vim.cmd("Neogit pull")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
 
-    vim.keymap.set( {"n"} , "<leader>gP",
+    vim.keymap.set({ "n" }, "<leader>gP",
       function()
         vim.cmd("Neogit push")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
 
-    vim.keymap.set( {"n"} , "<leader>gb",
+    vim.keymap.set({ "n" }, "<leader>gb",
       function()
         vim.cmd("Telescope git_branches")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
 
-    vim.keymap.set( {"n"} , "<leader>gB",
+    vim.keymap.set({ "n" }, "<leader>gB",
       function()
         vim.cmd("G blame")
       end,
-      { silent = true, noremap = true})
+      { silent = true, noremap = true })
   end
 }
