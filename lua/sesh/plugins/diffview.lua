02f24fca47fab90eb68b6ec1baf32a39e62a880b
seshonaar <andrei.balint@gmail.com>
2024-08-20 22:01:16 +0300

    DiffView: map ESC to toggle files panel instead of closing the session
    I found myself accidently closing the diff session by pressing ESC, so I
    remapped it to something harmless. Closing wih Q is enough.

--- lua/sesh/plugins/diffview.lua
+++ lua/sesh/plugins/diffview.lua
@@ -1,7 +1,27 @@
 return {
-  "seshonaar/diffview.nvim",        -- optional - Diff integration
+  "seshonaar/diffview.nvim", -- optional - Diff integration
   branch = "seshonaar/list_view_sort_by_impact",
   config = function()
-    vim.opt.fillchars:append({diff = ' '})
+    vim.opt.fillchars:append({ diff = ' ' })
+    -- Key mappings for DiffView
+    local actions = require("diffview.actions")
+
+    require("diffview").setup({
+      keymaps = {
+        -- Override ESC key to toggle the file explorer instead of closing the diff session
+        view = {
+          ["<esc>"] = actions.toggle_files,
+          ["q"] = "<cmd>DiffviewClose<CR>"
+        },
+        file_panel = {
+          ["<esc>"] = actions.toggle_files,
+          ["q"] = "<cmd>DiffviewClose<CR>"
+        },
+        file_history_panel = {
+          ["<esc>"] = actions.toggle_files,
+          ["q"] = "<cmd>DiffviewClose<CR>"
+        }
+      }
+    })
   end
 }
